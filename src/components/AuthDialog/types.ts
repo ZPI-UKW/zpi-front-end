3924613747435c9b79968a7df25166c9a1fe5447
Sebastian <sebastian.zawolanski@gmail.com>
2021-05-11 09:51:13 +0200

    feat: create dialog title

--- src/components/AuthDialog/types.ts
+++ src/components/AuthDialog/types.ts
@@ -1,6 +1,11 @@
 type ContentType = 'signin' | 'signup';
 export type SetContentType = React.Dispatch<React.SetStateAction<'signin' | 'signup'>>;
 
+export interface DialogTitleProps {
+  handleClose: () => void;
+  children: string;
+}
+
 export interface DialogContentProps {
   contentType: ContentType;
   setContentType: SetContentType;
