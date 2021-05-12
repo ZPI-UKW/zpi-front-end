465f0654cb63459d5c4cd489a8b726cea6cc8f67
Sebastian <sebastian.zawolanski@gmail.com>
2021-05-12 19:51:53 +0200

    feat: create dummy data

---
+++ src/data/annoucements.ts
@@ -0,0 +1,57 @@
+export interface Annoucements {
+  _id: string;
+  title: string;
+  desription: string;
+  addedBy: string;
+  location: string;
+  categoryId: string;
+  costs: {
+    day: number;
+    week: number;
+    month: number;
+  };
+  createdDate: string;
+  editedDate: string;
+  images: string[];
+}
+
+export const annoucements: Annoucements[] = [
+  {
+    _id: '1',
+    title: 'Wypożyczę Jelcza',
+    desription: 'Świetny jelcz! Niebieska sczała, śmiga jak szalony. Ładowny i szypki',
+    addedBy: '2',
+    location: 'Świecie',
+    categoryId: '1',
+    costs: {
+      day: 599,
+      week: 3499,
+      month: 14699,
+    },
+    createdDate: 'Wed May 10 2021 16:29:07 GMT+0200',
+    editedDate: 'Wed May 11 2021 12:04:19 GMT+0200',
+    images: [
+      'https://trailer.pl/wp-content/uploads/2018/03/Jelcz-315-e1522314770304.jpg',
+      'https://trailer.pl/wp-content/uploads/2018/03/Jelcz-315_2-1024x768.jpg',
+    ],
+  },
+  {
+    _id: '2',
+    title: 'Wypożyczę materac z sklepu Ikea',
+    desription:
+      'Ten średnio twardy materac ma strefy komfortu, które zmniejszają nacisk na biodra i barki, zapewniając wsparcie tam, gdzie tego potrzebujesz. Indywidualne sprężyny kieszeniowe pomogą Ci dobrze się wyspać i obudzić się wypoczętym.',
+    addedBy: '1',
+    location: 'Toruń',
+    categoryId: '2',
+    costs: {
+      day: 199,
+      week: 699,
+      month: 3299,
+    },
+    createdDate: 'Wed May 12 2021 16:29:07 GMT+0200',
+    editedDate: 'Wed May 12 2021 16:29:07 GMT+0200',
+    images: [
+      'https://www.strefasypialni.pl/userdata/public/gfx/d05827880a9c15f02639831948395bf7.jpg',
+    ],
+  },
+];
