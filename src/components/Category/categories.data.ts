4de60b6c59b8996a61e5710adc9f05efc98bcbfd
Pawel Sobkowski <psobkowski98@gmail.com>
2021-05-14 23:14:16 +0200

    chore: create dummy data

---
+++ src/components/Category/categories.data.ts
@@ -0,0 +1,42 @@
+import { categoryData } from '../../components/Category/category.interface';
+
+export const data: categoryData = {
+  categories: [
+    {
+      name: 'Pojazdy',
+      key: 'cars',
+    },
+    {
+      name: 'Elektronika',
+      key: 'electronics',
+    },
+    {
+      name: 'Narzędzia',
+      key: 'tools',
+    },
+    {
+      name: 'Moda',
+      key: 'fashion',
+    },
+    {
+      name: 'Książki',
+      key: 'books',
+    },
+    {
+      name: 'Sport',
+      key: 'sport',
+    },
+    {
+      name: 'Muzyka',
+      key: 'music',
+    },
+    {
+      name: 'Nieruchomości',
+      key: 'estate',
+    },
+    {
+      name: 'Filmy',
+      key: 'movies',
+    },
+  ],
+};
