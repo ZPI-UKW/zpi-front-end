2cabdc539a188f240d10ec7f3b4a997bd1ab9111
Pawel Sobkowski <psobkowski98@gmail.com>
2021-05-20 22:04:11 +0200

    feat: wrap children in grid item, import useStyles

--- src/views/MyRentals/myRentals.tsx
+++ src/views/MyRentals/myRentals.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import Card from '../../components/ProductCard/card';
 import { Annoucements, annoucements as ann } from '../../data/annoucements';
-import { CardsContainer } from '../../components/CardsContainer/cardsContainer';
+import CardsContainer from '../../components/CardsContainer';
 import ViewTitle from '../../components/ViewTitle';
 import ViewContainer from '../../components/ViewContainer';
+import { Grid } from '@material-ui/core';
+import useStyles from '../MyAnnoucements/styles';
 
 const MyRentals = () => {
   const [annoucements, setAnnoucements] = useState<Annoucements[]>([]);
+  const classes = useStyles();
 
   useEffect(() => {
     setAnnoucements(ann);
@@ -17,16 +20,18 @@ const MyRentals = () => {
       <ViewTitle>Moje wypozyczenia</ViewTitle>
       <CardsContainer>
         {annoucements.map((el) => (
-          <Card
-            variant="rentals"
-            key={el._id}
-            title={el.title}
-            price={el.costs.day}
-            _id={el._id}
-            images={el.images}
-            categoryId={el.categoryId}
-            location={el.location}
-          />
+          <Grid item xs={12} sm={6} md={4} lg={3} className={classes.gridItem}>
+            <Card
+              variant="rentals"
+              key={el._id}
+              title={el.title}
+              price={el.costs.day}
+              _id={el._id}
+              images={el.images}
+              categoryId={el.categoryId}
+              location={el.location}
+            />
+          </Grid>
         ))}
       </CardsContainer>
     </ViewContainer>
