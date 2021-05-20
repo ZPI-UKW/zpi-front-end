4a5925afe918e0a2139d53fc9af643c6363d1ad4
Pawel Sobkowski <psobkowski98@gmail.com>
2021-05-20 21:30:56 +0200

    feat: wrap children in grid item

--- src/views/MyAnnoucements/index.tsx
+++ src/views/MyAnnoucements/index.tsx
@@ -3,12 +3,13 @@ import Typography from '@material-ui/core/Typography';
 import { useEffect, useState } from 'react';
 import Card from '../../components/ProductCard/card';
 import { Annoucements, annoucements as ann } from '../../data/annoucements';
-import { CardsContainer } from '../../components/CardsContainer/cardsContainer';
+import CardsContainer from '../../components/CardsContainer';
 import AddIcon from '@material-ui/icons/Add';
 import { Link } from 'react-router-dom';
 import ViewTitle from '../../components/ViewTitle';
 import ViewContainer from '../../components/ViewContainer';
 import useStyles from './styles';
+import { Grid } from '@material-ui/core';
 
 const MyAnnoucements = () => {
   const [annoucements, setAnnoucements] = useState<Annoucements[]>([]);
@@ -22,26 +23,30 @@ const MyAnnoucements = () => {
     <ViewContainer>
       <ViewTitle>Moje ogłoszenia</ViewTitle>
       <CardsContainer>
+        <Grid item xs={12} sm={6} md={4} lg={3} className={classes.gridItem}>
+          <Link to="/create-advertisement" className={classes.addItemBox}>
+            <Box>
+              <AddIcon className={classes.plusIcon} />
+              <Typography color="textSecondary" variant="h4" component="span">
+                Dodaj ogłoszenie
+              </Typography>
+            </Box>
+          </Link>
+        </Grid>
         {annoucements.map((el) => (
-          <Card
-            variant="your"
-            key={el._id}
-            title={el.title}
-            price={el.costs.day}
-            _id={el._id}
-            images={el.images}
-            categoryId={el.categoryId}
-            location={el.location}
-          />
+          <Grid item xs={12} sm={6} md={4} lg={3} className={classes.gridItem}>
+            <Card
+              variant="your"
+              key={el._id}
+              title={el.title}
+              price={el.costs.day}
+              _id={el._id}
+              images={el.images}
+              categoryId={el.categoryId}
+              location={el.location}
+            />
+          </Grid>
         ))}
-        <Link to="/create-advertisement" className={classes.addItemBox}>
-          <Box>
-            <AddIcon className={classes.plusIcon} />
-            <Typography color="textSecondary" variant="h4" component="span">
-              Dodaj ogłoszenie
-            </Typography>
-          </Box>
-        </Link>
       </CardsContainer>
     </ViewContainer>
   );
