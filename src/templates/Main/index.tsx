8e909f7fa65f029aa1b6ecfd0078c4b25b9e6f06
Sebastian <sebastian.zawolanski@gmail.com>
2021-05-22 20:05:54 +0200

    feat: configure graphql connection

--- src/templates/Main/index.tsx
+++ src/templates/Main/index.tsx
@@ -1,15 +1,28 @@
+import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from '@material-ui/styles';
 import { AuthProvider } from '../../context/authContext';
 import GlobalStyles from '../../theme/globalStyles';
 import theme from '../../theme/material-theme';
 
+const link = createHttpLink({
+  uri: 'http://localhost:8080/graphql',
+  credentials: 'include',
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
+
 const MainTemplate = ({ children }: { children: React.ReactNode }) => (
-  <AuthProvider>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      {children}
-    </ThemeProvider>
-  </AuthProvider>
+  <ApolloProvider client={client}>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        {children}
+      </ThemeProvider>
+    </AuthProvider>
+  </ApolloProvider>
 );
 
 export default MainTemplate;
