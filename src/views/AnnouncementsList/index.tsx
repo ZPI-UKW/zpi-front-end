6f6490cdd68b334f055d2b7df20d98b437c3a9df
Pawel Sobkowski <psobkowski98@gmail.com>
2021-05-27 22:02:17 +0200

    feat: add breadcumbers

--- src/views/AnnouncementsList/index.tsx
+++ src/views/AnnouncementsList/index.tsx
@@ -4,21 +4,52 @@ import { Annoucements, annoucements as ann } from '../../data/annoucements';
 import CardsContainer from '../../components/CardsContainer';
 import ViewTitle from '../../components/ViewTitle';
 import ViewContainer from '../../components/ViewContainer';
+import { useLocation } from 'react-router';
+import { Breadcrumbs } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { Home } from '@material-ui/icons';
+import useStyles from './styles';
+import queryString from 'query-string';
 
 const AnnoucementsList = () => {
   const [annoucements, setAnnoucements] = useState<Annoucements[]>([]);
+  const location = useLocation();
+  const classes = useStyles();
+  const pathnames = location.pathname.split('/').filter((x) => x);
+  const searchParam = location.search ? queryString.parse(location.search).q : '';
 
   useEffect(() => {
     setAnnoucements(ann);
   }, []);
 
+  const renderSearch = (search: string): string => {
+    return ` - ${search}`;
+  };
+
+  const ifRenderSearch = (index: number, size: number, search: string): boolean => {
+    return index === size && search !== '';
+  };
+
   return (
     <ViewContainer>
-      <ViewTitle>Moje ogłoszenia</ViewTitle>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link to="/" className={classes.link}>
+          <Home />
+        </Link>
+        {pathnames &&
+          pathnames.map((path, index) => (
+            <span>
+              {path}
+              {ifRenderSearch(index, pathnames.length - 1, searchParam) &&
+                renderSearch(searchParam)}
+            </span>
+          ))}
+      </Breadcrumbs>
+      <ViewTitle>Wyniki wyszukiwania</ViewTitle>
       <CardsContainer>
         {annoucements.map((el) => (
           <Card
-            variant="your"
+            variant="home"
             key={el._id}
             title={el.title}
             price={el.costs.day}
