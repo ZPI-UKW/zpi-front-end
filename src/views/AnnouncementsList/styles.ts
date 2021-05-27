e45f13e7c3417cfa4eeb33eb10bb7dac8472721f
Pawel Sobkowski <psobkowski98@gmail.com>
2021-05-27 22:03:12 +0200

    feat: add styles for AnnouncementsList

---
+++ src/views/AnnouncementsList/styles.ts
@@ -0,0 +1,9 @@
+import { makeStyles, colors } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  link: {
+    color: colors.grey[900],
+  },
+}));
+
+export default useStyles;
