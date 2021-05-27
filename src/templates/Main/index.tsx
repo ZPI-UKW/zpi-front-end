75320cf8a43a15946d99d342c1cf3ef9aaaecd5c
Sebastian <sebastian.zawolanski@gmail.com>
2021-05-27 21:10:46 +0200

    feat: add DatePicker provider

--- src/templates/Main/index.tsx
+++ src/templates/Main/index.tsx
@@ -1,10 +1,14 @@
 import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from '@material-ui/styles';
+import MomentUtils from '@date-io/moment';
+import 'moment/locale/pl';
 import { AuthProvider } from '../../context/authContext';
 import GlobalStyles from '../../theme/globalStyles';
 import theme from '../../theme/material-theme';
 import { SnackbarProvider } from 'notistack';
 import useStyles from './styles';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import moment from 'moment';
 
 const link = createHttpLink({
   uri: 'http://localhost:8080/graphql',
@@ -20,14 +24,16 @@ const MainTemplate = ({ children }: { children: React.ReactNode }) => {
   const classes = useStyles();
   return (
     <ApolloProvider client={client}>
-      <AuthProvider>
-        <ThemeProvider theme={theme}>
-          <SnackbarProvider className={classes.contentRoot} maxSnack={3}>
-            <GlobalStyles />
-            {children}
-          </SnackbarProvider>
-        </ThemeProvider>
-      </AuthProvider>
+      <MuiPickersUtilsProvider utils={MomentUtils} locale={moment.locale('pl')}>
+        <AuthProvider>
+          <ThemeProvider theme={theme}>
+            <SnackbarProvider className={classes.contentRoot} maxSnack={3}>
+              <GlobalStyles />
+              {children}
+            </SnackbarProvider>
+          </ThemeProvider>
+        </AuthProvider>
+      </MuiPickersUtilsProvider>
     </ApolloProvider>
   );
 };
