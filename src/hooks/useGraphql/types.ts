dffef94f96c50a83bdc555b628cfa9c1124a8b02
Sebastian <sebastian.zawolanski@gmail.com>
2021-05-30 20:10:23 +0200

    feat: create useGraphql hook

---
+++ src/hooks/useGraphql/types.ts
@@ -0,0 +1,20 @@
+import { ApolloError } from '@apollo/client';
+
+export type OnError = (error: Error, status: number, message: string) => void;
+
+export interface DataControlProps<T> {
+  data: T | undefined | null;
+  error: ApolloError | undefined;
+  loading: boolean;
+  called: boolean;
+  onError?: OnError;
+  messages?: {
+    _401?: string;
+    _403?: string;
+    _404?: string;
+    _409?: string;
+    _422?: string;
+    _500?: string;
+  };
+  successMsg?: string;
+}
---
+++ src/hooks/useGraphql/useGraphql.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react';
+import { useSnackbar } from 'notistack';
+import { CustomApolloError } from '../../types/global';
+import { DataControlProps } from './types';
+
+const initialMessages = {
+  _401: 'Brak autoryzacji.',
+  _403: 'Dostęp do zasobu zabroniony.',
+  _404: 'Nie znaleziono zasobu.',
+  _409: 'Zasób już instnieje.',
+  _422: 'Niepoprawna wartość pól.',
+  _500: 'Wewnętrzny błąd serwera.',
+};
+
+export const useGraphql = <T,>({
+  data,
+  error,
+  loading,
+  called,
+  messages,
+  successMsg,
+  onError,
+}: DataControlProps<T>) => {
+  const { enqueueSnackbar } = useSnackbar();
+  const [isOk, setIsOk] = useState(false);
+  const [errorMessages, setErrorMessages] = useState(initialMessages);
+
+  useEffect(() => {
+    setErrorMessages({ ...errorMessages, ...messages });
+  }, []);
+
+  useEffect(() => {
+    if (!called || loading) return;
+
+    try {
+      if (data !== undefined && error === undefined) {
+        enqueueSnackbar(successMsg || 'Akcja wykonana pomyslnie.', {
+          variant: 'success',
+          autoHideDuration: 1500,
+        });
+        setIsOk(true);
+        console.log(error);
+      } else if (error !== undefined && error?.networkError) {
+        const err = new Error();
+        const { networkError } = error as CustomApolloError;
+
+        if (networkError?.result?.errors?.[0]?.status) {
+          const status = networkError.result.errors[0].status;
+          const resMessage = networkError.result.errors[0].message;
+          const msg = `_${status}` as const;
+
+          err.message = errorMessages[msg];
+
+          if (typeof onError === 'function') onError(err, status, resMessage || 'Wystąpił błąd');
+          setIsOk(false);
+          throw err;
+        }
+      } else {
+        setIsOk(false);
+        throw new Error('Wystąpił nieznany błąd.');
+      }
+    } catch (e) {
+      enqueueSnackbar(e.message || 'Wystąpił nieznany błąd.', {
+        variant: 'error',
+        autoHideDuration: 1500,
+      });
+    }
+  }, [error, data]);
+
+  return isOk;
+};
