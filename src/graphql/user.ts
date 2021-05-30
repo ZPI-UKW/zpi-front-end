a44d135d984c8f214c4f1b31ac650e1359a839cd
Sebastian <sebastian.zawolanski@gmail.com>
2021-05-30 20:10:48 +0200

    feat: create grapgql queries

---
+++ src/graphql/user.ts
@@ -0,0 +1,28 @@
+import { gql } from '@apollo/client';
+
+export const CHANGE_PASSWORD = gql`
+  mutation ChangePassword($currentPassword: String!, $newPassword: String!) {
+    changePassword(currentPassword: $currentPassword, newPassword: $newPassword) {
+      _id
+    }
+  }
+`;
+
+export const CHANGE_USER_DATA = gql`
+  mutation ChangeUserData(
+    $email: String!
+    $name: String!
+    $lastname: String!
+    $phonenumber: String!
+  ) {
+    changeUserData(
+      userInput: { email: $email, name: $name, lastname: $lastname, phonenumber: $phonenumber }
+    ) {
+      _id
+      email
+      name
+      lastname
+      phonenumber
+    }
+  }
+`;
