67906934998d947054ae87c86c02a1f6b267c349
zawolanski <sebastian.zawolanski@gmail.com>
2021-05-31 22:00:23 +0200

    refactor: remove redundant useEffect

--- src/components/DataControl/index.tsx
+++ src/components/DataControl/index.tsx
@@ -1,13 +1,8 @@
-import { useEffect } from 'react';
 import { DataControlProps } from '../../hooks/useGraphql/types';
 import { useGraphql } from '../../hooks/useGraphql/useGraphql';
 
 const DataControl = <T,>({ ...props }: DataControlProps<T>) => {
   useGraphql({ ...props });
-
-  useEffect(() => {
-    console.log('INSIDE');
-  }, []);
 
   return null;
 };
