e8b79db0cef209e40d5dfd6d96c2344e98567328
Sebastian <sebastian.zawolanski@gmail.com>
2021-06-15 20:55:05 +0200

    Revert "feat: move rent mutation variables to separate file"
    This reverts commit ecfcded85274d415926ea2795efab1b3aea4c40f.

--- src/components/RentDialog/index.tsx
+++ src/components/RentDialog/index.tsx
@@ -9,7 +9,6 @@ import DatePicker from '../CustomControls/datepicker.control';
 import DataControl from '../DataControl';
 import DialogTitle from '../DialogTitle';
 import SpinnerButton from '../SpinnerButton';
-import { RentVariables } from './helpers.rentdialog';
 import Pricing from './pricing.rentmodal';
 import { useStyles } from './styles';
 import { QueryData, QueryVars, RentDialogProps } from './types';
@@ -39,10 +38,16 @@ const RentDialog = ({ isOpen, handleClose, costs, id }: RentDialogProps) => {
             startDate: moment(),
             endDate: moment().add(1, 'days'),
           }}
-          onSubmit={async ({ endDate, startDate }) => {
+          onSubmit={async (values) => {
             try {
               setWasCalled(true);
-              await AddReservation(RentVariables(startDate, endDate, id));
+              await AddReservation({
+                variables: {
+                  endAt: moment(values.endDate).format('YYYY-MM-DD') + 'T00:00:00.000+00:00',
+                  startAt: moment(values.startDate).format('YYYY-MM-DD') + 'T00:00:00.000+00:00',
+                  annoucementId: id,
+                },
+              });
             } catch {}
           }}
         >
