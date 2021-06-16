b7c8c340efe1cc85837dcc28412bbea21b03246b
Sebastian <sebastian.zawolanski@gmail.com>
2021-06-16 15:32:24 +0200

    feat: add GET_ANNOUCEMENT_BY_ID query

--- src/graphql/annoucement.ts
+++ src/graphql/annoucement.ts
@@ -59,3 +59,26 @@ export const EDIT_ANNOUCEMENT = gql`
     }
   }
 `;
+
+export const GET_ANNOUCEMENT_BY_ID = gql`
+  query MyAnnoucement($id: String!) {
+    getAnnoucement(id: $id) {
+      id
+      title
+      description
+      location
+      categoryId {
+        id
+        name
+      }
+      phone
+      email
+      costs {
+        day
+        week
+        month
+      }
+      images
+    }
+  }
+`;
