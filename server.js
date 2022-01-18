30952403a83fe0010f6d7d41d5827e34ee7a9707
Sebastian <sebastian.zawolanski@gmail.com>
2022-01-18 21:48:52 +0100

    feat: update

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static('client/build'));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'build', 'index.html')));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
