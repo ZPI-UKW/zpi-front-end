77fdebeffeb93832c39ee7c9b7c4f2fa310398d6
Sebastian <sebastian.zawolanski@gmail.com>
2022-01-18 21:35:23 +0100

    refactor: change server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'build', 'index.html')));
-
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.use(express.static('client/build'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
